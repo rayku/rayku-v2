80eb92cfce2469b5949261ad401c5180bde60935
Colin Hutchinson <colin@hutchic.com>
2013-03-20 10:51:03 -0400

    User management view customization and tutors

--- src/Rayku/PageBundle/Controller/PageController.php
+++ src/Rayku/PageBundle/Controller/PageController.php
@@ -12,12 +12,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  */
 class PageController extends Controller
 {
+	/**
+	 * @Route("/dashboard", name="rayku_page_dashboard")
+	 */
+	public function dashboardAction()
+	{
+		return $this->render('RaykuPageBundle:Page:dashboard.html.twig');
+	}
 
 	/**
-	 * @Route("/", name="homepage")
+	 * @Route("/", name="rayku_page_homepage")
 	 */
 	public function indexAction()
 	{
 		return $this->render('RaykuPageBundle:Page:home.html.twig');
+	}
+	
+	/**
+	 * @Route("/about", name="rayku_page_about")
+	 */
+	public function aboutAction()
+	{
+		return $this->render('RaykuPageBundle:Page:about.html.twig');
+	}
+	
+	/**
+	 * @Route("/become-a-tutor", name="rayku_page_become_a_tutor")
+	 */
+	public function becomeTutorAction()
+	{
+		return $this->render('RaykuPageBundle:Page:become.html.twig');
 	}
 }
--- src/Rayku/TutorBundle/Controller/TutorController.php
+++ src/Rayku/TutorBundle/Controller/TutorController.php
@@ -2,12 +2,13 @@
 
 namespace Rayku\TutorBundle\Controller;
 
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Rayku\TutorBundle\Entity\Tutor;
+use Rayku\TutorBundle\Form\TutorType;
 
 /**
  * Tutor controller.
@@ -19,7 +20,7 @@ class TutorController extends Controller
     /**
      * Lists all Tutor entities.
      *
-     * @Route("/", name="tutor")
+     * @Route("/", name="rayku_tutor")
      * @Template()
      */
     public function indexAction()
@@ -36,7 +37,7 @@ class TutorController extends Controller
     /**
      * Finds and displays a Tutor entity.
      *
-     * @Route("/{id}/show", name="tutor_show")
+     * @Route("/{id}/show", name="rayku_tutor_show")
      * @Template()
      */
     public function showAction($id)
@@ -49,9 +50,150 @@ class TutorController extends Controller
             throw $this->createNotFoundException('Unable to find Tutor entity.');
         }
 
+        $deleteForm = $this->createDeleteForm($id);
+
         return array(
             'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
         );
     }
 
+    /**
+     * Displays a form to create a new Tutor entity.
+     *
+     * @Route("/new", name="rayku_tutor_new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Tutor();
+        $form   = $this->createForm(new TutorType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Tutor entity.
+     *
+     * @Route("/create", name="rayku_tutor_create")
+     * @Method("POST")
+     * @Template("RaykuTutorBundle:Tutor:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Tutor();
+        $form = $this->createForm(new TutorType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('tutor_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Tutor entity.
+     *
+     * @Route("/{id}/edit", name="rayku_tutor_edit")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RaykuTutorBundle:Tutor')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Tutor entity.');
+        }
+
+        $editForm = $this->createForm(new TutorType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Tutor entity.
+     *
+     * @Route("/{id}/update", name="rayku_tutor_update")
+     * @Method("POST")
+     * @Template("RaykuTutorBundle:Tutor:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('RaykuTutorBundle:Tutor')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Tutor entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new TutorType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('tutor_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Tutor entity.
+     *
+     * @Route("/{id}/delete", name="rayku_tutor_delete")
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('RaykuTutorBundle:Tutor')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Tutor entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('tutor'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
 }

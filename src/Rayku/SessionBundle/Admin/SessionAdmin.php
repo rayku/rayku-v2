5c13195b6d74cae7208eeab53126c665f0616289
Colin Hutchinson <colin@hutchic.com>
2013-03-20 11:36:54 -0400

    Re-add SessionBundle routing fix

---
+++ src/Rayku/SessionBundle/Admin/SessionAdmin.php
@@ -0,0 +1,58 @@
+<?php
+namespace Rayku\SessionBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class SessionAdmin extends Admin
+{
+    /*protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('username')
+            ->add('email')
+        ;
+    }*/
+
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+			->with('General')
+				->add('username')
+				->add('email')
+            	->add('duration')
+            	->add('rating')
+            	->add('rate')
+            	->add('question')
+            	//->add('start_time')
+            	//->add('end_time')
+            ->end()
+        ;
+	}
+	// Allows for filtering
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('duration')
+            ->add('rating')
+            ->add('rate')
+           	//->add('start_time')
+            //->add('end_time')
+        ;
+    }
+	protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+        	->add('Username')
+        	->add('Email')
+            ->addIdentifier('Duration')
+            ->addIdentifier('Rating')
+            ->addIdentifier('Rate')
+           	->addIdentifier('Question')
+           	//->addIdentifier('Start')
+            //->addIdentifier('End')
+        ;
+    }
+}
---
+++ src/Rayku/SessionBundle/RaykuSessionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rayku\SessionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RaykuSessionBundle extends Bundle
+{
+}

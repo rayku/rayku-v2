45715e9f5148e34b372f4728ca66d5278b1f680b
Colin Hutchinson <colin@hutchic.com>
2013-04-13 19:00:40 -0400

    Tutor request fix

--- src/Rayku/SessionBundle/Entity/SessionRepository.php
+++ src/Rayku/SessionBundle/Entity/SessionRepository.php
@@ -8,19 +8,18 @@ class SessionRepository extends EntityRepository
 {
 	public function findAllActiveByTutor($tutor_id, $expire_session)
 	{
-		$qb = $this->createQueryBuilder('s');
-		$query = $qb
-			->select(array('s'))
-			->from('\Rayku\SessionBundle\Entity\Session', 'session')
-			->innerJoin('session.potential_tutors', 't')
+		
+		$query = $this->createQueryBuilder('s')
+			->innerJoin('s.potential_tutors', 't')
 			->where('t.tutor = :tutorId')
 			->andWhere('t.tutorReply = \'pending\'')
-			->andWhere('session.selected_tutor is NULL')
-			->andWhere('session.endTime is NULL')
+			->andWhere('s.selected_tutor is NULL')
+			->andWhere('s.endTime is NULL')
 			->andWhere('s.createdAt > :expire_session')
 			->setParameter('tutorId', $tutor_id)
 			->setParameter('expire_session', date('Y-m-d H:i:s', strtotime($expire_session)))
 			->getQuery();
+
 		
 		return $query->getResult();
 		

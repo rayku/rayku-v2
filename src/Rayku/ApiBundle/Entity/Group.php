7525b8618c65e19b4f4a69ded0c729c5cca9c0e6
Colin Hutchinson <colin@hutchic.com>
2013-04-30 09:35:04 -0400

    Added path analytics

---
+++ src/Rayku/ApiBundle/Entity/Group.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MyProject\MyBundle\Entity;
+
+use FOS\UserBundle\Entity\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+     protected $id;
+     
+     /**
+      * @ORM\ManyToMany(targetEntity="MyProject\MyBundle\Entity\Group")
+      * @ORM\JoinTable(name="fos_user_user_group",
+      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+      *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+      * )
+      */
+     protected $groups;
+}

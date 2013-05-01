a9b9b4bd15fdbb29c8273fb913fdf18527c2ff57
Colin Hutchinson <colin@hutchic.com>
2013-05-01 14:33:10 -0400

    Fixed busy tutor bug query

--- src/Rayku/ApiBundle/Entity/TutorRepository.php
+++ src/Rayku/ApiBundle/Entity/TutorRepository.php
@@ -8,12 +8,21 @@ class TutorRepository extends EntityRepository
 {
 	public function findOnlineTutors($expire_online)
 	{
-		$query = $this->createQueryBuilder('t')
+		/**
+		 * WHERE ((r0_.online_web > ? OR r0_.online_gtalk > ?) AND (r0_.busy < ? OR r0_.busy IS NULL)) AND (r0_.deletedAt IS NULL)
+		 */
+		$qb = $this->createQueryBuilder('t');
+		$query = $qb
 			->select('t', 'u')
-			->where('t.onlineWeb > :expire_online')
-			->andWhere('t.busy < :expire_online')
 			->join('t.user', 'u')
-			->orWhere('t.onlineGtalk > :expire_online')
+			->where($qb->expr()->orx(
+				$qb->expr()->gt('t.onlineWeb', ':expire_online'),
+				$qb->expr()->gt('t.onlineGtalk', ':expire_online')
+			))
+			->andWhere($qb->expr()->orx(
+				$qb->expr()->lt('t.busy', ':expire_online'),
+				't.busy IS NULL'
+			))
 			->setParameter('expire_online', date("Y-m-d H:i:s", strtotime($expire_online)))
 			->getQuery();
 		

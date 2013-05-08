137813a8d57f2a1bd790dbf8f569d18faee84054
samrock06 <samrock06@gmail.com>
2013-05-08 12:55:13 -0400

    Sql for public profiles

---
+++ sql_for_public.sql
@@ -0,0 +1,24 @@
+update fos_user_user set image_path='http://i76.photobucket.com/albums/j26/drewdanburry/Danburry%20Barber%20Shop/IMG_2704.jpg' where email="j.barb@gmail.com";
+
+update fos_user_user set image_path='https://lh5.googleusercontent.com/GUjvfaL-bQcmr3ZMhB38LtP6JW2ouv5eiFcCa4frq31f16GoxnPZn1HnTQ496TIbudXA2qQQPmUT_w5jWk7O01SENlUhPkmctmlNw0os2A4GxCshQzoxlPni
+' where email="kim.p@gmail.com";
+
+update fos_user_user set image_path='https://lh6.googleusercontent.com/BkF71MEvIfQvJMlcHIYawXn-yNQUCIDAI0dXjNAwcpf0nx73y1YGLkLAPFpamzd8Vvz3bdi0qgnE-xt8TLYJYX-igvzbjYRY_lCX-P1BvJX7YDJYl2q4hYmt
+' where email="dnos@gmail.com";
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('4', 'Jason was a really cool guy. He took time to understand my question and situation before we started a session and taught me about systems of equations in
+ 15 minutes. ', 'Tnoah');
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('4', 'I wish teaching assistants at my school were as good as Jason. ', 'Wendy.lim');
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('4', 'Jason was great and helped me with trig. He made it interesting and was way easier to listen to than my math teacher.', 'Johnny95');
+
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('5', 'Very knowledgeable and already had a great sense of how to use the Rayku latex functions application which sped up my learning curve so I didn’t spend too much money! Glad to see tutors like this here!!!
+ ', 'sharifeltawil');
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('5', 'I used Rayku and Kim in the past for math help and asked for her opinion on my resume. She helped me re-write it on the whiteboard and it was honestly a life-saver. Applying for science jobs myself, it’s always good to hear from someone like Kim who has done the process before. ', 'siegel');
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('6', 'Great prof and better one-on-one support. He was able to paste a respiratory system onto the whiteboard and correct my labeling for my life sciences class. Being able to hear him rather than typing it out made my session so much faster. Only 10 minutes online would have taken me 2 days to book office hours and an hour in traveling (30mins to campus) and even then theres no guarantee my prof will be in or available.', 'hannah.kim');
+
+insert into rayku_review (tutor_id, review, reviewer_name) values('6', 'Dr. NOS! It always helps when profs take on technology because it helps us so much. I’m a commuter student so having profs online is so helpful because I can’t just “pop by”. The tools that the University uses (ahem Adobe Connect!) aren’t useful for technical classes. So thanks Dr. Nos for doing this for us!', 'drew.singer');

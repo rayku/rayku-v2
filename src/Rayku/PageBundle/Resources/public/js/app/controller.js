588b76e3ac7512b364e1601f2b94815be98cb0f0
Colin Hutchinson <colin@hutchic.com>
2013-06-11 22:47:29 -0400

    Loading page for ajax requests including creating a session

--- src/Rayku/PageBundle/Resources/public/js/app/controller.js
+++ src/Rayku/PageBundle/Resources/public/js/app/controller.js
@@ -1,60 +1,56 @@
-//'use strict';
+var app = angular.module('raykuApp', ['ngUpload', 'LoadingIndicator']);
 
-//define([], function () {
-    var app = angular.module('raykuApp', ['ngUpload']);
+//CONTROLLERS
+app.controller('TutorListCtrl', function ($scope, $rootScope, $http) {
+    //Online Tutors List Controller
+    $scope.tutorListTemplate = '/bundles/raykupage/js/app/views/TutorListView.html';
+	
+    $http.get(Routing.generate('get_tutors')).success(function(data) {
+        $scope.tutors = data;
+    });
 
-    //CONTROLLERS
-    app.controller('TutorListCtrl', function ($scope, $rootScope, $http) {
-        //Online Tutors List Controller
-        $scope.tutorListTemplate = '/bundles/raykupage/js/app/views/TutorListView.html';
-    	
+    $scope.refreshTutors = function(){
         $http.get(Routing.generate('get_tutors')).success(function(data) {
-            $scope.tutors = data;
+        	$scope.tutors = data;
         });
+    }
+    
+    $scope.update = function(user) {
+    	$http.post(Routing.generate('post_tutors'), user.tutor).success(function(data){
+    		$rootScope.user = user;
+    	})
+    }
+}).controller('SessionListCtrl',function ($scope, $http) {
+    //Sessions List Controller
+  	$scope.SessionListTemplate = '/bundles/raykupage/js/app/views/SessionsView.html';
 
-        $scope.refreshTutors = function(){
-            $http.get(Routing.generate('get_tutors')).success(function(data) {
-            	$scope.tutors = data;
-            });
-        }
-        
-        $scope.update = function(user) {
-        	$http.post(Routing.generate('post_tutors'), user.tutor).success(function(data){
-        		$('#myTutorModal').foundation('reveal', 'close');
-        		$rootScope.user = user;
-        	})
-        }
-    }).controller('SessionListCtrl',function ($scope, $http) {
-        //Sessions List Controller
-      	$scope.SessionListTemplate = '/bundles/raykupage/js/app/views/SessionsView.html';
+  	$http.get(Routing.generate('get_sessions', {'activeRequests':0})).success(function(data){
+  		$scope.sessions = data;
+  	});
+  	
+  	$scope.onLoad = function() {
+  	    $scope.loaded = true;
+  	}
+}).controller('UserDetailCtrl', function ($scope, $rootScope, $http){
+    //Users Details List Controller
+  	$scope.UserDetailTemplate = '/bundles/raykupage/js/app/views/ProfileView.html';
+    $scope.UsernameTemplate = '/bundles/raykupage/js/app/views/UsernameView.html';
 
-      	$http.get(Routing.generate('get_sessions', {'activeRequests':0})).success(function(data){
-      		$scope.sessions = data;
-      	});
-      	
-      	$scope.onLoad = function() {
-      	    $scope.loaded = true;
-      	}
-    }).controller('UserDetailCtrl', function ($scope, $rootScope, $http){
-        //Users Details List Controller
-      	$scope.UserDetailTemplate = '/bundles/raykupage/js/app/views/ProfileView.html';
-        $scope.UsernameTemplate = '/bundles/raykupage/js/app/views/UsernameView.html';
-
+  	$http.get(Routing.generate('get_user', {'entity':userId})).success(function(data){
+  		data.password = '';
+  		$rootScope.user = data;
+  	});
+  	
+    $scope.update = function(content, completed) {
       	$http.get(Routing.generate('get_user', {'entity':userId})).success(function(data){
       		$rootScope.user = data;
+      		$('.myprofile').click();
       	});
-      	
-        $scope.update = function(content, completed) {
-          	$http.get(Routing.generate('get_user', {'entity':userId})).success(function(data){
-          		$rootScope.user = data;
-          		$('.myprofile').click();
-          	});
-        }
+    }
 
-        $scope.profile = function(user) {
-        	$http.post(Routing.generate('post_users_profile', {'user':userId}), user).success(function(data){
-        		$rootScope.user = user;
-        	});
-        }
-    });  
-//});
+    $scope.profile = function(user) {
+    	$http.post(Routing.generate('post_users_profile', {'user':userId}), user).success(function(data){
+    		$rootScope.user = user;
+    	});
+    }
+});  

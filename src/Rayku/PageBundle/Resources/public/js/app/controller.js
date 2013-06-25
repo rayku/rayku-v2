261482a62276385a38e7a94635e353139b4671b6
samrock06 <samrock06@gmail.com>
2013-06-25 16:30:06 -0400

    Connect button fix

--- src/Rayku/PageBundle/Resources/public/js/app/controller.js
+++ src/Rayku/PageBundle/Resources/public/js/app/controller.js
@@ -2,19 +2,27 @@ var app = angular.module('raykuApp', ['ngUpload', 'LoadingIndicator']);
 
 //CONTROLLERS
 app.controller('TutorListCtrl', function ($scope, $rootScope, $http) {
-    var $table = angular.element(document.getElementById('tutorTable'));
-    var $trow = $table.find('tr');
-    console.log($trow);
-    $trow.click(function () {
-      console.log('clicked');
-      alert('clicked');
-      this.find('input[type="checkbox"]').attr("checked", "checked");
+    //Online Tutors List Controller
+    $scope.TutorListTemplate = '/bundles/raykupage/js/app/views/TutorListView.html';
+	
+    $http.get(Routing.generate('get_tutors')).success(function(data) {
+        $scope.tutors = data;
     });
-});
 
-app.controller('SessionListCtrl',function ($scope, $rootScope, $http) {
+    $scope.refreshTutors = function(){
+        $http.get(Routing.generate('get_tutors')).success(function(data) {
+        	$scope.tutors = data;
+        });
+    }
+    
+    $scope.update = function(user) {
+    	$http.post(Routing.generate('post_tutors'), user.tutor).success(function(data){
+    		$rootScope.user = user;
+    	})
+    }
+}).controller('SessionListCtrl',function ($scope, $rootScope, $http) {
     //Sessions List Controller
-  	$scope.SessionListTemplate = '/bundles/raykupage/js/app/views/SessionsView.html';
+    $scope.SessionListTemplate = '/bundles/raykupage/js/app/views/SessionsView.html';
     $scope.master= {};
 
     function refreshSessions(){
@@ -26,19 +34,18 @@ app.controller('SessionListCtrl',function ($scope, $rootScope, $http) {
     };
 
     //Should be used to update the sessions name
-    $scope.update = function (id) {
+    $scope.update = function (name) {
       //dont know what the url generated for session name update is so just wrote this in here as a placeholder
-      $http.post(Routing.generate('post_sessions', {'session':id}), data).success(function(data){
-        $rootScope.sessions = data;
+      $http.post(Routing.generate('post_sessions', {'session':5}), data).success(function(data){
         refreshSessions();
       }).error(function (data) {
         $scope.error = data || "Request failed";
       });
     }
-  	
-  	$scope.onLoad = function() {
-  	    $scope.loaded = true;
-  	}
+    
+    $scope.onLoad = function() {
+        $scope.loaded = true;
+    }
 
     refreshSessions();
 }).controller('UserDetailCtrl', function ($scope, $rootScope, $http){
@@ -47,18 +54,16 @@ app.controller('SessionListCtrl',function ($scope, $rootScope, $http) {
     $scope.UsernameTemplate = '/bundles/raykupage/js/app/views/UsernameView.html';
     $scope.TutorStatusTemplate = '/bundles/raykupage/js/app/views/TutorStatusView.html';
 
-  	$http.get(Routing.generate('get_user', {'entity':userId})).success(function (data){
+  	$http.get(Routing.generate('get_user', {'entity':userId})).success(function(data){
   		data.password = '';
   		$rootScope.user = data;
   	});
   	
     $scope.update = function(content, completed) {
-      	$http.get(Routing.generate('get_user', {'entity':userId})).success(function (data){
+      	$http.get(Routing.generate('get_user', {'entity':userId})).success(function(data){
       		$rootScope.user = data;
       		$('.myprofile').click();
-      	}).error(function (data) {
-          $scope.error = data || "Request failed";
-        });
+      	});
     }
 
     $scope.profile = function(user) {
@@ -67,43 +72,3 @@ app.controller('SessionListCtrl',function ($scope, $rootScope, $http) {
     	});
     }
 });  
-
-
-app.directive('tutorList', function () {
-    var ts = {
-      templateUrl: '/bundles/raykupage/js/app/views/TutorListView.html',
-      replace: true,
-      restrict: 'EACM',
-      scope: {}, 
-      controller: function ($scope, $rootScope, $http) {
-        //Online Tutors List Controller
-        $scope.tutorListTemplate = '/bundles/raykupage/js/app/views/TutorListView.html';
-        function refreshTutorlist () {
-          $http.get(Routing.generate('get_tutors')).success(function (data) {
-            $scope.tutors = data;
-          });
-        }
-
-        $scope.refreshTutors = function(){
-            $http.get(Routing.generate('get_tutors')).success(function (data) {
-              $scope.tutors = data;
-            });
-        }
-        
-        $scope.update = function(user) {
-          $http.post(Routing.generate('post_tutors'), user.tutor).success(function (data){
-            $rootScope.user = user;
-          })
-        }
-
-        refreshTutorlist();
-      }// end controller-
-    } // end ts
-  return ts;
-});
-
-
-
-
-
-

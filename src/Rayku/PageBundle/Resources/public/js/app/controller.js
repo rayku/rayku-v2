30ba9a4ce5b20c79b3a2cfdd911b0f85f56083f3
Colin Hutchinson <colin@hutchic.com>
2013-09-03 16:11:53 -0400

    Revert "math center redirect"
    This reverts commit ca61b06b24f50148ca5ae8d9789c0eedad1e9133.

--- src/Rayku/PageBundle/Resources/public/js/app/controller.js
+++ src/Rayku/PageBundle/Resources/public/js/app/controller.js
@@ -21,7 +21,7 @@ app.
 			when(routeGeneratorProvider.generate('angular_profile'), {templateUrl: '/bundles/raykupage/partials/user-edit.html'}).
 			when(routeGeneratorProvider.generate('angular_settings'), {templateUrl: '/bundles/raykupage/partials/user-settings.html'}).
 			when(routeGeneratorProvider.generate('angular_payout'), {templateUrl: '/bundles/raykupage/partials/payout.html'}).
-			otherwise({redirectTo:'/course/math_center/view'});
+			otherwise({redirectTo:routeGeneratorProvider.generate('rayku_username_dashboard', {username:username})});
 	}]);
 
 app.controller('CourseViewCtrl', function ($scope, $http, $routeParams){
